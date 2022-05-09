b725e1fa8fe472ca34932c967ddf7aa7807df911
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-09 20:20:15 -0300

    tipos de variáveis em C#

--- 001-tipos_de_dados_em_cSharp/001-tipos_de_dados_em_cSharp/Program.cs
+++ 001-tipos_de_dados_em_cSharp/001-tipos_de_dados_em_cSharp/Program.cs
@@ -6,8 +6,25 @@ namespace _001_tipos_de_dados_em_cSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            sbyte n1 = -128;
+            byte n2 = 255;
+            short n3 = -32768;
+            ushort n4 = 65535;
+            int n5 = -2147483648;
+            uint n6 = 4294967295;
+            long n7 = -9223372036854775808;
+            ulong n8 = 18446744073709551615;
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(n7);
+            Console.WriteLine(n8);
             
+
         }
     }
 }
