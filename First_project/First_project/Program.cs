461c5d70a4f1935f981cd803ab8c5217626328f3
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-09 19:14:50 -0300

    Teste

---
+++ First_project/First_project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace First_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name is Johnny and my years old is 31");
+        }
+    }
+}
