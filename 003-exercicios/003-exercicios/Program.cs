08e372d5479a47f12c90d48624803abb68226b3d
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-11 10:23:56 -0300

    Exercicios de fixação

---
+++ 003-exercicios/003-exercicios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace _003_exercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String produto1 = "Computador";
+            String produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double media = (preco1 + preco2) / 2;
+
+
+            // Saída de dados
+            Console.WriteLine("Produtos:");
+            Console.WriteLine(produto1 + ", cujo preço é $ " + preco1);
+            Console.WriteLine(produto2 + ", cujo preço é $ " + preco2);
+
+            Console.WriteLine("Registro: " + idade + " anos de idade, código " + codigo + " e genero " + genero);
+
+            Console.WriteLine("Média com 8 casas decimais: " + media.ToString("F8"));
+            Console.WriteLine("Arredondando para 3 casas decimais: " + media.ToString("F3"));
+            Console.WriteLine("Separador de casas decimais InvariantCulture: " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
