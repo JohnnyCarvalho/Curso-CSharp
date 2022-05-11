ecf04a8a3d13a34364f81ba16d344a46ad4194ef
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-11 12:50:50 -0300

    ex 005 criado

---
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/005-conversao_implicita_e_casting/005-conversao_implicita_e_casting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _005_conversao_implicita_e_casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
