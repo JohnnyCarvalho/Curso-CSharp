9642ffcd94b1597475b5003e3c704d2bd1ed4c31
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-17 09:37:15 -0300

    Entrada de dados parte 2

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/007-saida_de_dados/007-saida_de_dados/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/007-saida_de_dados/007-saida_de_dados/Program.cs
@@ -6,7 +6,28 @@ namespace _007_saida_de_dados
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             * Para entrada de dados em C# utilizamos o comando
+             * Consile.ReadLine();
+             * Vejamos os exemplos a seguir:
+             */
+
+            String frase = Console.ReadLine();
+            String x = Console.ReadLine();
+            String y = Console.ReadLine();
+            String z = Console.ReadLine();
+            
+            String[] vet = Console.ReadLine().Split(' ');
+            String a = vet[0];
+            String b = vet[1];
+            String c = vet[2];
+
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.Write(a + " " + b + " " + c);
+
         }
     }
 }
---
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008-entrada_de_dados_02/008-entrada_de_dados_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _008_entrada_de_dados_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
