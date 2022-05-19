1e5764ecc7e739a59a29df4da731c63be2b4bdaa
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-19 08:30:48 -0300

    aula 008
    entrada de dados parte 2

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008-entrada_de_dados_02/008-entrada_de_dados_02/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008-entrada_de_dados_02/008-entrada_de_dados_02/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _008_entrada_de_dados_02
 {
@@ -6,7 +7,13 @@ namespace _008_entrada_de_dados_02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"O valor de n1 é: {n1}");
+            Console.WriteLine($"O caractere digitado é: {ch}");
+            Console.WriteLine("O valor de n2 é: " + n2.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
