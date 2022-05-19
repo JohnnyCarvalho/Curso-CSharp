d3ae391caf9b685858bb463a8fd706c6c9b73233
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-19 09:09:21 -0300

    exercício de fixação

---
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008.1-exercicio_de_fixacao/008.1-exercicio_de_fixacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _008._1_exercicio_de_fixacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
