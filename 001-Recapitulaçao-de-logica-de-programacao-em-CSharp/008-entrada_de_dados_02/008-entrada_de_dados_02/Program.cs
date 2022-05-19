ff94386c8e1070b742abdc9a9885e70f5599f8d1
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-19 09:02:36 -0300

    Aula 008 complete

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008-entrada_de_dados_02/008-entrada_de_dados_02/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008-entrada_de_dados_02/008-entrada_de_dados_02/Program.cs
@@ -14,6 +14,16 @@ namespace _008_entrada_de_dados_02
             Console.WriteLine($"O valor de n1 é: {n1}");
             Console.WriteLine($"O caractere digitado é: {ch}");
             Console.WriteLine("O valor de n2 é: " + n2.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("\nAgora veremos a segunda parte do projeto de hoje:");
+
+            String[] vet = Console.ReadLine().Split(' ');
+            String nome = vet[0];
+            Char sexo = char.Parse(vet[1]);
+            int idade = int.Parse(vet[2]);
+            Double peso = double.Parse(vet[3], CultureInfo.InvariantCulture);
+            
+            Console.WriteLine("Nome: "+nome+"; Sexo: "+sexo+"; Idade: "+idade+"; Peso: "+peso.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
