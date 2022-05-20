3c85be933ee9b2a75c7d4bbbb50e6cb081eeff21
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-19 22:02:31 -0300

    exercício concluído

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008.1-exercicio_de_fixacao/008.1-exercicio_de_fixacao/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/008.1-exercicio_de_fixacao/008.1-exercicio_de_fixacao/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _008._1_exercicio_de_fixacao
 {
@@ -6,7 +7,36 @@ namespace _008._1_exercicio_de_fixacao
     {
         static void Main(string[] args)
         {
-            //exercício de fixação
+            /*
+             * Fazer um programa que receba os dados do usuário armazene os mesmo em variáveis
+             * e na sequência imprima na tela os dados armazenados dentro de cada variável.
+             */
+
+            Console.WriteLine("What's your complete name?");
+            String completeName = Console.ReadLine();
+
+            Console.WriteLine("How many roons do you have in your house?");
+            int roons = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter with product price:");
+            double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter with your last name, weight and height (write on same line)");
+            String[] vet = Console.ReadLine().Split(' ');
+            String lastName = vet[0];
+            int weight = int.Parse(vet[1]);
+            double height = double.Parse(vet[2], CultureInfo.InvariantCulture);
+
+            // Saída de dados
+
+            Console.WriteLine($"Your complete name is: {completeName}.");
+            Console.WriteLine($"Your house has {roons} roons.");
+            Console.WriteLine("The value of product is: "+price.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine($"Yous last name is {lastName}, your years old is {weight} and yous height is "+
+                height.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            
 
         }
     }
