ef82f60bf5025445bb9d32747c364886566eec9b
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-24 09:06:12 -0300

    comentários

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/001-ex/001-ex/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/001-ex/001-ex/Program.cs
@@ -1,5 +1,26 @@
 ﻿using System;
 
+/*
+ * Faça um programa para ler dois valores inteiros, e 
+ * depois mostrar na tela a soma desses números com uma
+ * mensagem explicativa, conforme exemplos.
+ * 
+ * Exemplos:
+
+Entrada:              |          Saída:
+10                    |          SOMA = 40
+30                    |  
+                      |  
+Entrada:              |          Saída:
+-30                   |          SOMA = -20
+                      |  
+10                    |  
+Entrada:              |          Saída:
+0                     |          SOMA = 0
+0                     |  
+
+ */
+
 namespace _001_ex
 {
     internal class Program
