6a451e3329cdbffac47acd7fccccf0673a2acc4a
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-24 08:57:14 -0300

    ex 001 resolvido

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/001-ex/001-ex/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/001-ex/001-ex/Program.cs
@@ -6,7 +6,22 @@ namespace _001_ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = 10;
+            int num2 = 30;
+            int num3 = -30;
+            int num4 = 10;
+            int num5 = 0;
+            int num6 = 0;
+
+
+            int soma1 = num1 + num2;
+            int soma2 = num3 + num4;
+            int soma3 = num5 + num6;
+
+            Console.WriteLine($"SOMA = {soma1}");
+            Console.WriteLine($"SOMA = {soma2}");
+            Console.WriteLine($"SOMA = {soma3}");
+
         }
     }
 }
