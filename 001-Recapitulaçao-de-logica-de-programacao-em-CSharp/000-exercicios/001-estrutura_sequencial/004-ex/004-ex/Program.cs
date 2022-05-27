5ac3cc12a7c76b877b23d388ee2a1c1d19e23ea9
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-27 00:28:04 -0300

    EX 004 INICIADO

---
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/004-ex/004-ex/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+
+
+namespace _004_ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
