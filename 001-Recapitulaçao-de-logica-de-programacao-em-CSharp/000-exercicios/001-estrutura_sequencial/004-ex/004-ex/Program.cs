121ce962d46533d5289a6f82cb77d7e03ecc0fd9
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-27 09:57:47 -0300

    commit

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/004-ex/004-ex/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/004-ex/004-ex/Program.cs
@@ -1,7 +1,27 @@
 ﻿using System;
 using System.Globalization;
 
+/*
+ * Fazer um programa que leia o número de um funcionário, 
+ * seu número de horas trabalhadas, o valor que recebe por
+ * hora e calcula o salário desse funcionário. A seguir, mostre 
+ * o número e o salário do funcionário, com duas casas decimais.
+ * 
+ * Exemplos:
 
+Entrada:              |          Saída:
+25                    |          NUMBER = 25
+100                   |          SALARY = U$ 550.00
+5.50                  |  
+Entrada:              |          Saída:
+1                     |          SOMA = 1
+200                   |          SALARY = U$ 4100.00
+20.5                  |  
+Entrada:              |          Saída:
+6                     |          SOMA = 6
+145                   |          SALARY = U$ 2254.75
+15.55                 |
+ */
 
 namespace _004_ex
 {
