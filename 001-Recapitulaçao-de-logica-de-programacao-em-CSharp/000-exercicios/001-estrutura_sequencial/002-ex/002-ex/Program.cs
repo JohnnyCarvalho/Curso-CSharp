9bbfd12f647f64c509c34060aa79b64030dc8f81
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-05-27 00:00:56 -0300

    Exercicio 2

--- 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/002-ex/002-ex/Program.cs
+++ 001-Recapitulaçao-de-logica-de-programacao-em-CSharp/000-exercicios/001-estrutura_sequencial/002-ex/002-ex/Program.cs
@@ -1,4 +1,28 @@
 ﻿using System;
+using System.Globalization;
+
+/*
+ * Faça um programa para ler o valor do raio de um círculo, 
+ * e depois mostrar o valor da área deste círculo com quatro
+ * casas decimais conforme exemplos.
+ * Fórmula da área: area = π . raio2
+ * Considere o valor de π = 3.14159
+ * 
+ * Exemplos:
+
+Entrada:              |     Saída:
+2.0                   |     A = 12.5664
+                      |  
+                      |  
+Entrada:              |     Saída:
+100.64                |     A = 31819.3103
+                      |  
+                      |  
+Entrada:              |     Saída:
+150.00                |     A = 70685.7750
+                      |  
+
+ */
 
 namespace _002_ex
 {
@@ -6,7 +30,21 @@ namespace _002_ex
     {
         static void Main(string[] args)
         {
-            
+            Double pi = 3.14159;
+
+            Double raio1 = 2.0;
+            Double raio2 = 100.64;
+            Double raio3 = 150.00;
+
+            Double area1 = Math.Pow(raio1, 2) * pi;
+            Double area2 = Math.Pow(raio2, 2) * pi;
+            Double area3 = Math.Pow(raio3, 2) * pi;
+
+            Console.WriteLine("Area 1 = "+area1.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area 2 = " + area2.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area 3 = "+area3.ToString("F4", CultureInfo.InvariantCulture));
+
+
         }
     }
 }
